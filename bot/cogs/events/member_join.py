64773a36fc341f99403819f399dfc556ce4b8d61
TreltaSev <treltasev@gmail.com>
2023-08-02 22:14:53 -0400

    Updated member join/remove

--- bot/cogs/events/member_join.py
+++ bot/cogs/events/member_join.py
@@ -5,7 +5,7 @@ event: `on_member_join`
 
 Displays a custom "banner" on member join
 """
-
+import discord
 from discord.ext import commands
 from exts.constants.cogs import Setup
 
@@ -15,7 +15,7 @@ class on_member_join(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_member_join(self):
+    async def on_member_join(self, member: discord.Member):
         pass
 
 @Setup.basic(on_member_join, "Event:on_member_join")
--- bot/cogs/events/member_remove.py
+++ bot/cogs/events/member_remove.py
@@ -6,18 +6,19 @@ event: `on_member_leave`
 Displays a custom "banner" on member leave
 """
 
+import discord
 from discord.ext import commands
 from exts.constants.cogs import Setup
 
-class on_member_leave(commands.Cog):
+class on_member_remove(commands.Cog):
     
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
 
     @commands.Cog.listener()
-    async def on_member_leave(self):
+    async def on_member_remove(self, member: discord.Member):
         pass
 
-@Setup.basic(on_member_leave, "Event:on_member_leave")
+@Setup.basic(on_member_remove, "Event:on_member_leave")
 async def setup(client: commands.Bot):
     pass
