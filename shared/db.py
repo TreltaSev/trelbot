6c7dca935cd35ab3baccbe48f25fe07fa1618530
TreltaSev <treltasev@gmail.com>
2023-08-04 00:39:42 -0400

    Staged shared

---
+++ shared/db.py
@@ -0,0 +1,14 @@
+"""
+shared.db
+~~~~~~~~~
+Access local databases with sqlite3 package
+"""
+import sqlite3
+
+
+class Settings:
+    """
+    Reads, Writes and Saves data within discord.guild.settings.db
+    takes in a guild_id    
+    """
+    pass
