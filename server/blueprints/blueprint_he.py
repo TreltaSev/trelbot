3a6d3d162df08440b29ce004d19f3369fd0b4b8c
TreltaSev <treltasev@gmail.com>
2023-08-09 00:10:04 -0400

    Added 2 error types removed 1

--- server/blueprints/blueprint_he.py
+++ server/blueprints/blueprint_he.py
@@ -1,8 +1,2 @@
 import quart
 from exts.constants.App import BlueprintsManager
-
-blueprint = quart.Blueprint("api:test", __name__, subdomain="test")
-
-@blueprint.route("/test")
-async def test_route():
-    return "Test indeed"
