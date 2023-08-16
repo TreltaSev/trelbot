a482e614a87cc5e5d2899731b78528144e14a77e
TreltaSev <treltasev@gmail.com>
2023-08-16 18:52:56 -0400

    Updated app.tsx

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "@routes/login";
 import DiscordCallback from "@routes/discord-callback";
+import Dashboard from "@routes/dashboard";
 
 /* Main router that controls everything being displayed depending on the route. */
 const App: React.FC = () => {
@@ -11,6 +12,10 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/discord-callback" element={<DiscordCallback/>}/>
+                    <Route path="/dashboard">
+                        <Route path=":guildId" element={<Dashboard/>}/>
+                        <Route path="" element={<Dashboard/>}/>
+                    </Route>
                 </Routes>
             </Router>
         </>
