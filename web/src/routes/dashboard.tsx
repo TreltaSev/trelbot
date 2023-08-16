f251fb6c833c2eb65eb46028159d66fd6181ee55
TreltaSev <treltasev@gmail.com>
2023-08-16 18:51:41 -0400

    Updated Dashboard.tsx

--- web/src/routes/dashboard.tsx
+++ web/src/routes/dashboard.tsx
@@ -4,8 +4,12 @@
  */
 
 import React from "react"
+import { useParams } from "react-router-dom"
 
 const Dashboard: React.FC = () => {
+
+    let { guildId } = useParams();
+    
     return (
         <div>
             Dashboard
