267698b5f14cb83ef01d2a140408e004acdac9dd
TreltaSev <treltasev@gmail.com>
2023-08-16 18:50:08 -0400

    Created dashboard.tsx

---
+++ web/src/routes/dashboard.tsx
@@ -0,0 +1,16 @@
+/**
+ * Path: www.local.xyz/dashboard
+ * Description: Dashboard, look in github
+ */
+
+import React from "react"
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard
