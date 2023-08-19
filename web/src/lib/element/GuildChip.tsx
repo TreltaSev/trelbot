869e9c247a9cdddd325ee30ebceb17a026cf2e5e
TreltaSev <treltasev@gmail.com>
2023-08-19 18:25:08 -0400

    Added noshrink

--- web/src/lib/element/GuildChip.tsx
+++ web/src/lib/element/GuildChip.tsx
@@ -12,7 +12,7 @@ interface GuildChipProperties {
 const GuildChip: React.FC<GuildChipProperties> = ({ name, image, display, present}) => {
     return (
         <div style={{minWidth: 800, minHeight: 80, padding: 20, gap: 10}} className={`${styling.flex_row} ${styling.align_items_center} ${styling.borderbox}`}>
-            <img alt="" src={image} width={40} height={40} style={{borderRadius: "50%"}}/>
+            <img alt="" src={image} width={40} height={40} style={{borderRadius: "50%"}} className={`${styling.no_shrink}`}/>
         </div>
     )
 }
