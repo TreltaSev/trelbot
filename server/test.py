32ea2c9c4ac6f92be5993eafdd8c200f24c15671
TreltaSev <treltasev@gmail.com>
2023-08-19 16:01:33 -0400

    Reset

--- server/test.py
+++ server/test.py
@@ -3,16 +3,3 @@ import sys
 # Prevent program from creating __pycache__ dirs
 sys.dont_write_bytecode = True
 sys.path.append(os.path.abspath(f"{os.getcwd()}/../"))
-from exts.constants import oauth2
-
-
-
-try:
-    _code = "t8PrhaGVSWvNUlzejuvob5MZllYbS1"
-    _user = oauth2.Oauth2.GetCurrentUser(_code)
-    print(_user.__dict__)
-except Exception as e:
-    if hasattr(e, "jsonstr"):
-        print(e.json)
-    else:
-        raise e
