9795f63a1fb32c4931053e1fca5b12bc330058b8
TreltaSev <treltasev@gmail.com>
2023-08-20 19:56:37 -0400

    Added sizes.tsx file

---
+++ web/src/lib/types/sizes.tsx
@@ -0,0 +1,23 @@
+const _form = (desktop: number, phone: number, tablet?: number): number => {
+    const _phoneThreshold: number = 500;
+    const _desktopThreshold: number = 1280;
+
+    // Width within desktop threshold
+    if (window.innerWidth >= _desktopThreshold) {
+        return desktop;
+    }
+
+    // Width within phone threshold
+    if (window.innerWidth <= _phoneThreshold) {
+        return phone;
+    }    
+    
+    if (tablet === undefined) {
+        tablet = phone;
+    }
+
+    // Width between 500 and 1280 meaning tablet
+    return tablet;
+}
+
+const text_normal: number = _form(16, 20, 18);
