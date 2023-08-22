c21664e478ab84bc99ee6c20f5fc57d10abbae2e
TreltaSev <treltasev@gmail.com>
2023-08-22 08:34:54 -0400

    Updated Arrow.tsx

--- web/src/lib/svg/Arrow.tsx
+++ web/src/lib/svg/Arrow.tsx
@@ -10,7 +10,7 @@ const Arrow: React.FC<TypeArrow> = ({ width, height }) => {
   width = defaultValue(width, 14, undefined);
   height = defaultValue(height, 14, undefined);
   return (
-    <div style={{width: width, height: height}}>
+    <div style={{ width: width, height: height }}>
       <svg width='100%' height='100%' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
         <path d='M6.99995 9.07082L3.5437 5.62915H10.4562L6.99995 9.07082Z' fill='white' />
       </svg>
