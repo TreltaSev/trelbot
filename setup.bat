38283a59393d37088d94d0a1d2f621ef5aba7362
TreltaSev <treltasev@gmail.com>
2023-08-22 09:00:50 -0400

    Updated setup files

--- setup.bat
+++ setup.bat
@@ -3,3 +3,4 @@ call ./venv/Scripts/activate.bat
 pip install -r requirements.txt
 cd ./web
 npm i
+cd ./../
--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
-Start-Process -FilePath "./venv/Scripts/Activate.ps1"
+./venv/Scripts/Activate.ps1
 pip install -r ./requirements.txt
 Set-Location -Path ./Web
 npm i
+Set-Location -Path ./../
