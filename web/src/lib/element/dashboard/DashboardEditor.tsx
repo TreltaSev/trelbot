ffbbdc2679c8b45c3e0e2e69380fc1e7bbc4452d
TreltaSev <treltasev@gmail.com>
2023-08-24 17:10:41 -0400

    Update web

--- web/src/lib/element/dashboard/DashboardEditor.tsx
+++ web/src/lib/element/dashboard/DashboardEditor.tsx
@@ -1,4 +1,5 @@
 import fetch_guild from "@root/lib/method/fetch_guild";
+import guild from "@root/lib/types/guild";
 import React, { useEffect } from "react";
 
 type type_DashboardEditor = {
@@ -8,7 +9,9 @@ type type_DashboardEditor = {
 const DashboardEditor: React.FC<type_DashboardEditor> = ({guild_id}) => {
   useEffect(() => {
     // Load Data
-    fetch_guild(guild_id)
+    fetch_guild(guild_id).then((guild_data: guild) => {
+      console.log(guild_data)
+    })
   }, [])
   return <div>Editor</div>;
 };
