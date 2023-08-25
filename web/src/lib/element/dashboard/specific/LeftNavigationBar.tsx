fd8952162cb25dd37bd74ed192295b6a00f1838b
TreltaSev <treltasev@gmail.com>
2023-08-25 08:54:37 -0400

    Updated Web

--- web/src/lib/element/dashboard/specific/LeftNavigationBar.tsx
+++ web/src/lib/element/dashboard/specific/LeftNavigationBar.tsx
@@ -4,6 +4,7 @@ import FlexColumn from "@lib/element/FlexColumn";
 import { get } from "@lib/element/dashboard/loader";
 import guild from "@lib/types/guild";
 import GuildContainer from "@lib/element/dashboard/specific/GuildContainer";
+import SettingContainer from "./SettingContainer";
 
 type type_LeftNavigationBar = {
   guild: guild | undefined
@@ -14,6 +15,7 @@ const LeftNavigationBar: React.FC<type_LeftNavigationBar> = ({ guild }) => {
   return (
     <FlexColumn style={{width: 250, gap:10, padding: "0 10px 0 10px", flexShrink: 0}} className={`${styling.fill_height} ${styling.align_items_center} ${styling.border_box} ${styling.dark}`}>
       <GuildContainer guild={guild}/>
+      <SettingContainer name="Automations"/>
     </FlexColumn>
   );
 }
