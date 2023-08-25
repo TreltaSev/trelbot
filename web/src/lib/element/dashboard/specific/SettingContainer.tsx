5a4e243cb0a9b1d88f856faa6244ecebd1c9d733
TreltaSev <treltasev@gmail.com>
2023-08-25 08:41:59 -0400

    Updated Web

--- web/src/lib/element/dashboard/specific/SettingContainer.tsx
+++ web/src/lib/element/dashboard/specific/SettingContainer.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styling from "@assets/styling.module.css";
 import component from "@root/lib/types/component";
+import FlexRow from "@lib/element/FlexRow";
+import Text from "@lib/element/Text";
 
 type type_SettingContainer = component & {
   name?: string;  
   icon?: string;
 }
 
-const SettingContainer: React.FC<type_SettingContainer> = ({ guild }) => {
+const SettingContainer: React.FC<type_SettingContainer> = ({ name, icon }) => {
   return (
-    <>
-      <></>
-    </>
+    <FlexRow style={{gap: 10, padding:10, borderRadius: 5}} className={`${styling.align_items_center} ${styling.darksub}`}>
+      <Text preset="1em-normal" style={{color: "white", fontSize: 16}}>{name}</Text>
+    </FlexRow>
   );
 };
 
