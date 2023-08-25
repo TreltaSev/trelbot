e0749289031064f41e71dc604beae1455b20e324
TreltaSev <treltasev@gmail.com>
2023-08-25 13:29:28 -0400

    Update Web

--- web/src/lib/element/ToggleButton.tsx
+++ web/src/lib/element/ToggleButton.tsx
@@ -35,27 +35,29 @@ class ToggleButton extends React.Component<props_ToggleButton, state_ToggleButto
       this.props.ontoggle();
     }
 
-    if (!this.innerRef.current) {
+    if (!this.innerRef.current || !this.toggleRef.current) {
       return;
     }
 
     let _nv: boolean | undefined = defaultValue(!this.state.active, this.state.initial, undefined);
     this.innerRef.current.animate({ transform: `translateX(${boolValue(_nv, 30, 4)}px)` }, { duration: 300, fill: "forwards", easing: "cubic-bezier(.11, .07, .04, .98)" });
+    this.toggleRef.current.animate({ background: boolValue(_nv, "#8C52FF", "rgba(255,255,255,0.2)") }, { duration: 300, fill: "forwards", easing: "cubic-bezier(.11, .07, .04, .98)" });
     this.setState({ active: _nv });
   }
 
   componentDidMount(): void {
     const _v: boolean = defaultValue(this.props.initialValue, false, undefined);
 
-    if (this.innerRef.current) {
+    if (this.innerRef.current && this.toggleRef.current) {
       this.innerRef.current.style.transform = `translateX(${boolValue(_v, 30, 4)}px)`;
+      this.toggleRef.current.style.background = boolValue(_v, "#8C52FF", "rgba(255,255,255,0.2)");
     }
   }
 
   render() {
     return (
       <div onClick={() => this.toggle()} ref={this.toggleRef} style={{ background: "#E45853", width: 50, height: 20, position: "relative", borderRadius: 5, cursor: "pointer" }}>
-        <div ref={this.innerRef} style={{ width: 16, height: 16, background: "rgba(255,255,255,0.5)", borderRadius: 5, position: "absolute", top: 2 }} />
+        <div ref={this.innerRef} style={{ width: 16, height: 16, background: "rgba(255,255,255,0.5)", borderRadius: 5, position: "absolute", top: 2, border: "4ps solid rgba(255,255,255,0.5)" }} />
       </div>
     );
   }
