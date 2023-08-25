bf42771275dc334a8e3ffcc468f08eb1421ba8b5
TreltaSev <treltasev@gmail.com>
2023-08-25 10:42:43 -0400

    Update Web

--- web/src/lib/element/dashboard/pages/automations/index.tsx
+++ web/src/lib/element/dashboard/pages/automations/index.tsx
@@ -4,10 +4,12 @@ import OnJoin from "@lib/element/dashboard/pages/automations/OnJoin";
 import OnLeave from "@lib/element/dashboard/pages/automations/OnLeave";
 import { loader, register_parent } from "@lib/element/dashboard/loader";
 import FlexColumn from "@root/lib/element/FlexColumn";
+import styling from "@assets/styling.module.css";
+
 
 const Automations: React.FC = () => {
   return (
-    <FlexColumn>
+    <FlexColumn style={{width: 800, padding: 10, gap: 25}} className={`${styling.align_items_flex_start} ${styling.border_box}}`}>
       <OnJoin/>
       <OnLeave/>
       <OnBan/>
