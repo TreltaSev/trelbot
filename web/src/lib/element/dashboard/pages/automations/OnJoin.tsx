bbf884aaaf8c09ca50e6ecb5152303d2070df2ea
TreltaSev <treltasev@gmail.com>
2023-08-25 11:20:39 -0400

    Update Web

--- web/src/lib/element/dashboard/pages/automations/OnJoin.tsx
+++ web/src/lib/element/dashboard/pages/automations/OnJoin.tsx
@@ -1,7 +1,20 @@
+import FlexColumn from "@lib/element/FlexColumn";
+import TextGroup from "@lib/element/TextGroup";
+import Text from "@lib/element/Text"
 import React from "react";
 
 const OnJoin: React.FC = () => {
-  return <>On Join</>;
+  return (
+    <>
+    <FlexColumn>
+      <TextGroup>
+        <Text>On Join Event</Text>
+        <Text>Sends some sort of message based on the configuration below</Text>
+      </TextGroup>
+    </FlexColumn>
+    
+    </>
+  );
 };
 
 export default OnJoin;
