ff6b0d0e87578fc630e4a16b0a019ffa9836d0ae
TreltaSev <treltasev@gmail.com>
2023-08-25 14:11:42 -0400

    Updated UsernameGroup.tsx

--- web/src/lib/element/UsernameGroup.tsx
+++ web/src/lib/element/UsernameGroup.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styling from "@assets/styling.module.css";
-import DepracatedText from "@root/lib/element/DepracatedText";
-import user from "../types/user";
+import Text from "@lib/element/Text";
+import user from "@lib/types/user";
 
 interface UsernameGroupProps {
   user: user;
@@ -13,10 +13,10 @@ const UsernameGroup: React.FC<UsernameGroupProps> = ({ user }) => {
   }
   return (
     <div className={`${styling.flex_row}`}>
-      <DepracatedText size={14}>{user.name}</DepracatedText>
-      <DepracatedText size={14} opacity='0.5'>
+      <Text preset="normal" style={{fontSize: 14}}>{user.name}</Text>
+      <Text preset="normal" style={{fontSize: 14, opacity: "0.5"}}>
         #{user.discriminator}
-      </DepracatedText>
+      </Text>
     </div>
   );
 };
