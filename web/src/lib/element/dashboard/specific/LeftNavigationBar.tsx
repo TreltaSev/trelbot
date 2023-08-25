9c1f3016c3d39c23018d2929c66067bb28d2cc36
TreltaSev <treltasev@gmail.com>
2023-08-24 23:48:02 -0400

    Update web

--- web/src/lib/element/dashboard/specific/LeftNavigationBar.tsx
+++ web/src/lib/element/dashboard/specific/LeftNavigationBar.tsx
@@ -3,6 +3,7 @@ import styling from "@assets/styling.module.css";
 import FlexColumn from "@lib/element/FlexColumn";
 import { get } from "@lib/element/dashboard/loader";
 import guild from "@lib/types/guild";
+import GuildContainer from "@lib/element/dashboard/specific/GuildContainer";
 
 type type_LeftNavigationBar = {
   guild: guild | undefined
@@ -12,7 +13,7 @@ const LeftNavigationBar: React.FC<type_LeftNavigationBar> = ({ guild }) => {
   console.log(get())
   return (
     <FlexColumn style={{width: 250, gap:10, padding: "0 10px 0 10px", flexShrink: 0}} className={`${styling.fill_height} ${styling.align_items_center} ${styling.border_box} ${styling.dark}`}>
-      
+      <GuildContainer guild={guild}/>
     </FlexColumn>
   );
 }
