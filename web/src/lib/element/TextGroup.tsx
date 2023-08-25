e02c9bd1b8a9758ba4bcf41daf5c079c219f599b
TreltaSev <treltasev@gmail.com>
2023-08-24 23:21:26 -0400

    Update web

---
+++ web/src/lib/element/TextGroup.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import FlexColumn from "./FlexColumn";
+
+type type_TextGroup = {
+  children?: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const TextGroup: React.FC<type_TextGroup> = ({children, className, style}) => {
+  return (
+    <FlexColumn style={style} className={className}>
+      {children}
+    </FlexColumn>
+  )
+}
+
+export default TextGroup;
--- web/src/lib/element/dashboard/specific/GuildContainer.tsx
+++ web/src/lib/element/dashboard/specific/GuildContainer.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 import styling from "@assets/styling.module.css";
-import FlexColumn from "@lib/element/FlexColumn";
 import { get } from "@lib/element/dashboard/loader";
 import guild from "@lib/types/guild";
+import FlexRow from "@lib/element/FlexRow";
+
+type type_GuildContainer = {
+  guild: guild | undefined
+}
+
+const GuildContainer: React.FC<type_GuildContainer> = ({ guild }) => {
+  return (
+    <FlexRow style={{padding: 10, gap: 10}} className={`${styling.align_items_center} ${styling.align_self_stretch}`}>
+      {/* Image */}
+
+      
+    </FlexRow>
+  )
+}
