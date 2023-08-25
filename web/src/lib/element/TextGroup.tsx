bc776840e5650779e383cb2b6a8742531ed9e8cb
TreltaSev <treltasev@gmail.com>
2023-08-24 23:21:40 -0400

    Update web

--- web/src/lib/element/TextGroup.tsx
+++ web/src/lib/element/TextGroup.tsx
@@ -5,14 +5,14 @@ type type_TextGroup = {
   children?: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
-}
+};
 
-const TextGroup: React.FC<type_TextGroup> = ({children, className, style}) => {
+const TextGroup: React.FC<type_TextGroup> = ({ children, className, style }) => {
   return (
     <FlexColumn style={style} className={className}>
       {children}
     </FlexColumn>
-  )
-}
+  );
+};
 
 export default TextGroup;
