658c1f8d6e075f5c32f492b9107778bdc3634a67
TreltaSev <treltasev@gmail.com>
2023-08-27 19:58:56 -0400

    Created LoadingAnimated Component

---
+++ web/src/lib/element/dashboard/specific/LoadingAnimated.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styling from "@assets/styling.module.css";
+import component from "@root/lib/types/component";
+
+const LoadingAnimated: React.FC<component> = ({className, style}) => {
+  return (
+    <></>
+  )
+}
+
+export default LoadingAnimated;
