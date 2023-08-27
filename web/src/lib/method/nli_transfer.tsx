a7c3a2378e60eb0b1d22d9f58616ec23705d0e5e
TreltaSev <treltasev@gmail.com>
2023-08-27 16:05:56 -0400

    Updated nli_transfer.tsx

--- web/src/lib/method/nli_transfer.tsx
+++ web/src/lib/method/nli_transfer.tsx
@@ -3,7 +3,7 @@
  * Transfers the client back to /login if use isn't logged in
  */
 
-import set from "@root/lib/method/loginAction";
+import {set as loginSet} from "@lib/method/loginAction";
 
 /**
  * Sends a user back to login if input value is undefined
@@ -12,7 +12,7 @@ import set from "@root/lib/method/loginAction";
  */
 const nli_transfer = (value: any): void => {
   if (value === undefined) {
-    set("error", "You dont seem to be logged in, or maybe you cleared your cookies.", "1020", "/login");
+    loginSet("error", "You dont seem to be logged in, or maybe you cleared your cookies.", "1020", "/login");
   }
   return;
 };
