55fa983659a3e162bee3b8f58dbbfbe72575ec21
TreltaSev <treltasev@gmail.com>
2023-08-27 22:24:50 -0400

    Updated animation component

--- web/src/lib/element/dashboard/specific/LoadingAnimated.tsx
+++ web/src/lib/element/dashboard/specific/LoadingAnimated.tsx
@@ -1,13 +1,39 @@
 import React from "react";
-import component from "@lib/types/component";
 import FlexRow from "@lib/element/FlexRow";
 import FlexColumn from "@lib/element/FlexColumn";
 
 import scss_LoadingAnimated from "@assets/LoadingAnimated.module.scss";
 
-const LoadingAnimated: React.FC<component> = ({ className, style }) => {
+type type_LoadingAnimated = {
+  size?: "small" | "medium" | "large";
+};
+
+const LoadingAnimated: React.FC<type_LoadingAnimated> = ({ size }) => {
+  if (!size) {
+    size = "small";
+  }
+  let heightvalue: string = "0";
+  let widthvalue: number = 0;
+  let sizeclass: string = "";
+  switch (size) {
+    case "small":
+      sizeclass = scss_LoadingAnimated.small;
+      widthvalue = 35;
+      heightvalue = "0.5em";
+      break;
+    case "medium":
+      sizeclass = scss_LoadingAnimated.medium;
+      widthvalue = 45;
+      heightvalue = "0.75em";
+      break;
+    case "large":
+      sizeclass = scss_LoadingAnimated.large;
+      widthvalue = 70;
+      heightvalue = "1em";
+      break;
+  }
   return (
-    <FlexRow style={{height: "calc(0.5em + 10px)", width: "35px" }} className={`${scss_LoadingAnimated.animationcontainer} ${scss_LoadingAnimated.small}`}>
+    <FlexRow style={{ height: `calc(${heightvalue} + 10px)`, width: `${widthvalue}px` }} className={`${scss_LoadingAnimated.animationcontainer} ${sizeclass}`}>
       <FlexColumn />
       <FlexColumn />
       <FlexColumn />
