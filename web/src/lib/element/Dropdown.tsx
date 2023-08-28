5c0d62e6120b2cddad25ffb3ece703cf888850a3
TreltaSev <treltasev@gmail.com>
2023-08-28 13:37:16 -0400

    Update Dropdown

--- web/src/lib/element/Dropdown.tsx
+++ web/src/lib/element/Dropdown.tsx
@@ -1,27 +1,41 @@
 import React from "react";
 import styling from "@assets/styling.module.css";
 import component from "@lib/types/component";
-import FlexRow from "./FlexRow";
+import FlexRow from "@lib/element/FlexRow";
 import ChannelTag from "@lib/svg/ChannelTag";
 import Text from "@lib/element/Text";
 import Spacer from "@lib/element/Spacer";
-import Arrow from "../svg/Arrow";
+import Arrow from "@lib/svg/Arrow";
+import defaultValue from "@lib/method/defaultValue";
 
-type type_dropdown = component & {};
-
-const Dropdown: React.ForwardRefRenderFunction<HTMLDivElement, type_dropdown> = () => {
-  return (
-    <>
-      <FlexRow style={{ width: 300, height: 30, flexShrink: 0, padding: "0 10px", gap: 10, borderRadius: 5 }} className={`${styling.align_items_center} ${styling.dark}`}>
-        <ChannelTag style={{minWidth: 16, minHeight: 16, width: 16, height: 16}}/>
-        <Text preset='1em-normal' style={{ opacity: "0.8", whiteSpace: "nowrap" }}>
-          Select a Channel
-        </Text>
-        <Spacer />
-        <Arrow style={{minWidth: 20, minHeight: 20, width: 20, height: 20}} />
-      </FlexRow>
-    </>
-  );
+type props_Dropdown = {
+  name?: string;
 };
 
-export default Dropdown
+type state_Dropdown = {};
+
+class Dropdown extends React.Component<props_Dropdown, state_Dropdown> {
+  private _name = defaultValue(this.props.name, "Select", undefined);
+
+  constructor(props: props_Dropdown) {
+    super(props);
+    this.state = {} as state_Dropdown;
+  }
+
+  render() {
+    return (
+      <>
+        <FlexRow style={{ width: 300, height: 30, flexShrink: 0, padding: "0 10px", gap: 10, borderRadius: 5 }} className={`${styling.align_items_center} ${styling.dark}`}>
+          <ChannelTag style={{ minWidth: 16, minHeight: 16, width: 16, height: 16 }} />
+          <Text preset='1em-normal' style={{ opacity: "0.8", whiteSpace: "nowrap" }}>
+            Select a Channel
+          </Text>
+          <Spacer />
+          <Arrow style={{ minWidth: 20, minHeight: 20, width: 20, height: 20 }} />
+        </FlexRow>
+      </>
+    );
+  }
+}
+
+export default React.forwardRef((props: props_Dropdown, ref: React.Ref<Dropdown>) => <Dropdown {...props} ref={ref} />);
