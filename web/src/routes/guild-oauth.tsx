5cef389b6b847edb7936ff6b1dbd9c5c0e9a890c
TreltaSev <treltasev@gmail.com>
2023-08-28 09:21:13 -0400

    Update guild-oauth.tsx

--- web/src/routes/guild-oauth.tsx
+++ web/src/routes/guild-oauth.tsx
@@ -3,6 +3,9 @@
  * Description: Discord callback handler for guild invites
  */
 import React, { useEffect } from "react";
+import FlexRow from "@root/lib/element/FlexRow";
+import styling from "@assets/styling.module.css";
+import LoadingAnimated from "@root/lib/element/LoadingAnimated";
 
 const GuildCallback = () => {
   const guild_id: string | null = new URLSearchParams(window.location.search).get("guild_id");
@@ -15,7 +18,11 @@ const GuildOauth: React.FC = () => {
   useEffect(() => {
     GuildCallback();
   });
-  return <></>;
+  return (
+    <FlexRow className={`${styling.fill_all} ${styling.align_items_center} ${styling.justify_content_center} ${styling.dark}`}>
+      <LoadingAnimated size={30} gap={15} heightoffset={20} />
+    </FlexRow>
+  );
 };
 
 export default GuildOauth;
