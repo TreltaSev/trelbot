8bf1bcf26ab9059ba62e2497f1224f33632f32e5
TreltaSev <treltasev@gmail.com>
2023-08-28 13:00:17 -0400

    Update OnJoin.tsx

--- web/src/lib/element/dashboard/pages/automations/OnJoin.tsx
+++ web/src/lib/element/dashboard/pages/automations/OnJoin.tsx
@@ -1,6 +1,6 @@
 import FlexColumn from "@lib/element/FlexColumn";
 import TextGroup from "@lib/element/TextGroup";
-import Text from "@lib/element/Text"
+import Text from "@lib/element/Text";
 import React from "react";
 import ToggleButton from "@root/lib/element/ToggleButton";
 
@@ -8,14 +8,17 @@ const OnJoin: React.FC = () => {
   const toggleRef = React.useRef(null);
   return (
     <>
-    <FlexColumn style={{gap: 20}}>
-      <TextGroup>
-        <Text preset="1.5em-normal" style={{color: "white", fontWeight: "700"}}>On Join Event</Text>
-        <Text preset="1.5em-normal" style={{color: "white", opacity: "0.5"}}>Sends some sort of message based on the configuration below</Text>
-      </TextGroup>
-      <ToggleButton ref={toggleRef} ontoggle={() => console.log("heheheha")}/>
-    </FlexColumn>
-    
+      <FlexColumn style={{ gap: 20 }}>
+        <TextGroup>
+          <Text preset='1.5em-normal' style={{ color: "white", fontWeight: "700" }}>
+            On Join Event
+          </Text>
+          <Text preset='1.5em-normal' style={{ color: "white", opacity: "0.5" }}>
+            Sends some sort of message based on the configuration below
+          </Text>
+        </TextGroup>
+        <ToggleButton ref={toggleRef} ontoggle={() => console.log("heheheha")} />
+      </FlexColumn>
     </>
   );
 };
