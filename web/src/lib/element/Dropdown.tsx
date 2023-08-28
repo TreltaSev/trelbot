b7b18aa1d68176aaa46ee0043a4b69df031b99f1
TreltaSev <treltasev@gmail.com>
2023-08-28 13:05:19 -0400

    Channel

---
+++ web/src/lib/element/Dropdown.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styling from "@assets/styling.module.css";
+import component from "@lib/types/component";
+import FlexRow from "./FlexRow";
+
+type type_dropdown = component & {
+
+}
+
+const Dropdown: React.ForwardRefRenderFunction<HTMLDivElement, component> = () => {
+  return (
+    <>
+      <FlexRow>
+      
+      </FlexRow>
+    </>
+  )
+}
