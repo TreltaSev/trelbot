39298e09262b0badb08cb0a3d7b985af2251393c
TreltaSev <treltasev@gmail.com>
2023-08-28 09:06:34 -0400

    Update cache@me.tsx

--- web/src/lib/method/cache@me.tsx
+++ web/src/lib/method/cache@me.tsx
@@ -4,17 +4,27 @@ import errorCatcher from "@lib/method/errorCatcher";
 import nli_transfer from "@lib/method/nli_transfer";
 import user from "@lib/types/user";
 import userSession from "@lib/method/userSession";
+import loginAction from "./loginAction";
 
 const cache_me = async (set: (user: user) => void): Promise<user> => {
   const session = Cookies.get("session");
   nli_transfer(session);
 
   const _request: RequestInit = { method: "get", headers: { Session: session as string } };
-  const _meResponse = await fetch(`${config.backendUrl}/@me`, _request);
-  const _me = await _meResponse.json();
+  let _me: any = undefined;
+  try{
+    const _meResponse = await fetch(`${config.backendUrl}/@me`, _request);
+    _me = await _meResponse.json();
+  } catch (e) {
+    new userSession().remove()
+    new loginAction().setError("error", "Error Fetching Backend", "1020", "/login")
+    throw new Error(`Response contains error for @me in json ${_me}`);
+  }
+  
 
   if (errorCatcher(_me)) {
     new userSession().remove()
+    new loginAction().setError("error", "Error Parsing Backend Response", "1020", "/login")
     throw new Error(`Response contains error for @me in json ${_me}`);
   }
 
