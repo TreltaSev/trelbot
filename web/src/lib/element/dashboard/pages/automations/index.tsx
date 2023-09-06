b6c541a3b8c6fa50000ef3317faacb40076d186b
TreltaSev <treltasev@gmail.com>
2023-09-05 20:54:47 -0400

    index of automations.tsx

--- web/src/lib/element/dashboard/pages/automations/index.tsx
+++ web/src/lib/element/dashboard/pages/automations/index.tsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OnBan from "@lib/element/dashboard/pages/automations/OnBan";
 import OnJoin from "@lib/element/dashboard/pages/automations/OnJoin";
 import OnLeave from "@lib/element/dashboard/pages/automations/OnLeave";
 import { loader, register_parent } from "@lib/element/dashboard/loader";
 import FlexColumn from "@root/lib/element/FlexColumn";
 import styling from "@assets/styling.module.css";
+import cache_channels from "@root/lib/method/cache@channels";
+import channel from "@lib/types/channel";
+import { useParams } from "react-router-dom";
 
-export const channels = React.createContext<null>(null);
+export const channels = React.createContext<channel[] | undefined>(undefined);
 export const roles = React.createContext<null>(null);
 
 const Automations: React.FC = () => {
+  let { guildId } = useParams();
+  const [v_channels, setChannels] = useState<channel[] | undefined>(undefined);
+
+  useEffect(() => {
+    cache_channels(guildId, setChannels)
+  }, []);
+
   return (
-    <FlexColumn style={{width: 800, padding: 30, gap: 25, borderRadius: 5}} className={`${styling.align_items_flex_start} ${styling.darksub} ${styling.border_box}`}>
-      <OnJoin/>
-      <OnLeave/>
-      <OnBan/>
-    </FlexColumn>
-  )
-}
+    <channels.Provider value={v_channels}>
+      <FlexColumn style={{ width: 800, padding: 30, gap: 25, borderRadius: 5 }} className={`${styling.align_items_flex_start} ${styling.darksub} ${styling.border_box}`}>
+        <OnJoin />
+        <OnLeave />
+        <OnBan />
+      </FlexColumn>
+    </channels.Provider>
+  );
+};
 
 register_parent("Automations", undefined);
 loader("automations", <Automations />, "Automations", 1);
-
--- web/src/lib/method/cache@channels.tsx
+++ web/src/lib/method/cache@channels.tsx
@@ -3,13 +3,14 @@ import config from "@assets/config";
 import errorCatcher from "@lib/method/errorCatcher";
 import nli_transfer from "@lib/method/nli_transfer";
 import channel from "@lib/types/channel";
+import { strnum } from "@lib/types/sizes";
 
-const cache_channels = async (set?: (channels: channel[]) => void): Promise<channel[]> => {
+const cache_channels = async (guild_id: strnum, set?: (channels: channel[]) => void): Promise<channel[]> => {
   const session = Cookies.get("session");
   nli_transfer(session);
 
   const _request: RequestInit = { method: "get", headers: { Session: session as string} };
-  const _channelsResponse = await fetch(`${config.backendUrl}/guilds/{guild_id}/channels`)
+  const _channelsResponse = await fetch(`${config.backendUrl}/guilds/${guild_id}/channels`, _request)
   const _channels = await _channelsResponse.json();
 
   console.log(_channels)
