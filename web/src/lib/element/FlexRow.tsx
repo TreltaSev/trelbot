26de4fe24628329ca589c33c2ba75c282a28bf1c
TreltaSev <treltasev@gmail.com>
2023-09-06 22:38:02 -0400

    Update FlexRow.tsx

--- web/src/lib/element/FlexRow.tsx
+++ web/src/lib/element/FlexRow.tsx
@@ -1,13 +1,13 @@
-import React, { useRef } from "react";
-import styling from "@assets/styling.module.css";
+import React from "react";
 import component from "@lib/types/component";
+import styling from "@assets/styling.module.css";
 
-const FlexRow: React.ForwardRefRenderFunction<HTMLDivElement, component> = ({children, className, style, onMouseEnter, onMouseLeave, onClick, innerref}) => {
+const FlexRow: React.ForwardRefRenderFunction<HTMLDivElement, component> = ({ children, className, style, onMouseEnter, onMouseLeave, onClick, innerref }) => {
   return (
     <div ref={innerref} onClick={onClick} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} style={style} className={`${styling.flex_row} ${className}`}>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default FlexRow
+export default FlexRow;
