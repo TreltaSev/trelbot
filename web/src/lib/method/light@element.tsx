3986b2bc4456805bf4acc4cac24ec02d4e0b40ab
TreltaSev <treltasev@gmail.com>
2023-09-06 22:31:48 -0400

    Updated light@element.tsx

--- web/src/lib/method/light@element.tsx
+++ web/src/lib/method/light@element.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 
 const light_element = (ref: React.RefObject<HTMLDivElement>): Animation | undefined => {
   if (!ref.current) {
-    return
+    return;
   }
-  return ref.current.animate({opacity: "1"}, {duration: 300, fill: "forwards", easing: "cubic-bezier(.11, .07, .04, .98)"})
-}
+  return ref.current.animate({ opacity: "1" }, { duration: 300, fill: "forwards", easing: "cubic-bezier(.11, .07, .04, .98)" });
+};
 
-export default light_element
+export default light_element;
