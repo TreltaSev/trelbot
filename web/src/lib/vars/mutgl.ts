19f5c6b6b8958de5c53478c170fe53c6319e7831
TreltaSev <treltasev@gmail.com>
2023-10-02 19:32:18 -0400

    Update/mutgl.ts

--- web/src/lib/vars/mutgl.ts
+++ web/src/lib/vars/mutgl.ts
@@ -1,5 +1,6 @@
-import config from "@root/config";
 import Cookies from "js-cookie";
+import user from "@lib/types/user";
+import config from "@root/config";
 
 /**
  * Mutable Global Variables, holds user information
@@ -9,12 +10,14 @@ import Cookies from "js-cookie";
 class _mutgl {
   public db = {};
   public guilds = {};
-  public user = {};
+  public user: user = {};
 
   /**
    * This method sends a request to the backend api, asking for the users information like
    * avatar urls, usernames and the like and has an optional parameter for automating "caching" which
    * saves this data to this classes public value: `user`
+   *
+   * @param _cache `Literal[True, False]` If set to true, caches the data to this.user, Defaults to false
    */
   public rc_user = async (_cache: boolean = false): Promise<any> => {
     const _session = Cookies.get("session");
@@ -34,12 +37,36 @@ class _mutgl {
       return;
     }
 
+    if (this.error_c(_me)) {
+      return;
+    }
+
     if (_cache) {
       this.user = _me;
     }
 
-    console.log(_me);
     return _me;
+  };
+
+  /**
+   * Private method that checks an object for error codes
+   * that would be given from backend api
+   * @param input the value you want to check
+   * @returns A boolean, if its true theres an error, if not there isn't
+   */
+  private error_c = (input: any): boolean => {
+    if (input.hasOwnProperty("code")) {
+      switch (input.code) {
+        case 1020:
+        case 1028:
+          // Error found 1020/1028
+          break;
+        default:
+          console.error(`Error, Stack: ${input}`);
+          return true;
+      }
+    }
+    return false;
   };
 }
 
