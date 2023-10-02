28b51f0883157683710811ae3459366a9e731b41
TreltaSev <treltasev@gmail.com>
2023-10-02 19:20:41 -0400

    Update/Selector.tsx

--- web/src/routes/dashboard/selector/Selector.tsx
+++ web/src/routes/dashboard/selector/Selector.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Selector: React.FC = () => {
-
   {
     /**
      * This useEffect should loop every 500ms to check if guilds have or have
      * not been loaded.
      */
   }
+  useEffect(() => {}, []);
   return (
     <>
       <></>
