2793ac3609cfdfe0d2d8be4e7953ec82c0f331da
TreltaSev <treltasev@gmail.com>
2023-10-05 19:11:41 -0400

    Testing/method:rc_guild()

--- web/src/routes/dashboard/editor/Editor.tsx
+++ web/src/routes/dashboard/editor/Editor.tsx
@@ -1,20 +1,21 @@
+import mutgl from "@root/lib/vars/mutgl";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const Editor: React.FC = () => {
-  
   let { guildId } = useParams();
 
   /**
    * @useEffect Fetches the guild data from the guild id by sending
-   * a get request to the backend api, that response gets saved into a 
+   * a get request to the backend api, that response gets saved into a
    * guild object inside const guild.
    */
   useEffect(() => {
-
     /* Fetch and Save Guild Data like channels and settings */
-
-  }, [])
+    mutgl.rc_guild(guildId as string).then((response) => {
+      console.log(response);
+    });
+  }, []);
 
   return (
     <>
