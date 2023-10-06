d8907541db81dd952da7b85eff23dd6d563f711f
TreltaSev <treltasev@gmail.com>
2023-10-05 23:06:06 -0400

    Create/file:NavigationTab.tsx

---
+++ web/src/routes/dashboard/editor/NavigaionTab.tsx
@@ -0,0 +1,8 @@
+import FlexColumn from "@lib/component/FlexColumn";
+import React from "react";
+
+const NavigationTab: React.FC = () => {
+  return <FlexColumn></FlexColumn>;
+};
+
+export default NavigationTab;
