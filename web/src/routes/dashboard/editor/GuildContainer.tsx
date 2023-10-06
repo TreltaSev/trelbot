fdcdc54b22d2d95f0f660781a7dfb763adc6d0c6
TreltaSev <treltasev@gmail.com>
2023-10-06 16:36:07 -0400

    Update/file:GuildContainer.tsx

--- web/src/routes/dashboard/editor/GuildContainer.tsx
+++ web/src/routes/dashboard/editor/GuildContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import mutgl from "@lib/vars/mutgl";
 import Url from "@lib/component/Url";
 import Text from "@lib/component/Text";
@@ -9,24 +9,28 @@ import FlexColumn from "@lib/component/FlexColumn";
 import LoadingAnimated from "@lib/component/LoadingAnimated";
 
 const GuildContainer: React.FC = () => {
+  // Force update used when refreshing metaguild
+  const forceUpdate = useReducer((x) => x + 1, 0)[1];
+
   // Multi used values for simplicity
   const main_style: React.CSSProperties = { paddingLeft: 10, paddingRight: 10, gap: 10, borderRadius: 5, height: 65, flexShrink: 0 };
   const main_class: string = `${styling.align_items_center} ${styling.align_self_stretch} ${styling.darksub}`;
 
-  // Get cached meta data from guild object
-  const metaGuild = mutgl.cGuild.meta;
-
-  console.log(metaGuild);
-
   /**
-   * ! Stoped Here
-   * TODO: Implement looper to check metaGuild
-   * Since the data is loaded only once and since
-   * I refuse to use `useContext` I should create a method
-   * that loops until a value equals something else.
+   * @useEffect creates an interval that runs every 500 ms and that interval
+   * checks if metaGuild is undefined, if it is continue about its day, if not it
+   * sets the state of a value to save the current guild. this is used instead of context api
    */
+  useEffect(() => {
+    const __ = setInterval(() => {
+      if (Object.keys(mutgl.cGuild.meta).length >= 1) {
+        clearInterval(__);
+        forceUpdate();
+      }
+    }, 500);
+  }, []);
 
-  if (Object.keys(metaGuild).length == 0) {
+  if (Object.keys(mutgl.cGuild.meta).length == 0) {
     // Return a loading animation or smth
     return (
       <FlexRow style={main_style} className={`${main_class} ${styling.justify_content_center}`}>
@@ -41,14 +45,14 @@ const GuildContainer: React.FC = () => {
       {/**
        * Image
        */}
-      <img alt='' src={metaGuild.icon_url} style={{ borderRadius: "50%" }} width={40} height={40} />
+      <img alt='' src={mutgl.cGuild.meta.icon_url} style={{ borderRadius: "50%" }} width={40} height={40} />
 
       <FlexColumn style={{ gap: 5 }}>
         {/**
          * Name Container
          */}
         <Text preset='1em-normal' style={{ color: "white", textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden", width: 150 }}>
-          {metaGuild.name}
+          {mutgl.cGuild.meta.name}
         </Text>
 
         {/**
