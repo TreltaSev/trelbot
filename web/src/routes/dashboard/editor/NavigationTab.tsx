aa214e834ffe26787b9460643bc59675fc07a2c2
TreltaSev <treltasev@gmail.com>
2023-10-05 23:09:31 -0400

    Update/File:NavigationTab.tsx

--- web/src/routes/dashboard/editor/NavigationTab.tsx
+++ web/src/routes/dashboard/editor/NavigationTab.tsx
@@ -1,6 +1,10 @@
 import FlexColumn from "@lib/component/FlexColumn";
 import React from "react";
 
+/**
+ * @displays A navigation tab containing the guild information and inner buttons that link to other tabs.
+ * @usedin Editor.tsx
+ */
 const NavigationTab: React.FC = () => {
   return <FlexColumn></FlexColumn>;
 };
