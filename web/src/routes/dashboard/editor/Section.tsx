cc6f0f525ec8cddf4db549a3956bb78a14ad8241
TreltaSev <treltasev@gmail.com>
2023-10-07 19:53:28 -0400

    Update/file:Section.tsx

--- web/src/routes/dashboard/editor/Section.tsx
+++ web/src/routes/dashboard/editor/Section.tsx
@@ -3,11 +3,15 @@ import Text from "@lib/component/Text";
 import identifier from "@lib/types/identifier";
 import FlexColumn from "@root/lib/component/FlexColumn";
 
+import styling from "@assets/styling.module.css";
+
 const Section: React.FC<identifier> = ({ name, description, children }) => {
   return (
-    <FlexColumn>
-      <Text>{name}</Text>
-      <Text>{description}</Text>
+    <FlexColumn style={{ gap: 10 }} className={styling.align_items_flex_start}>
+      <Text preset='1.5em-bold'>{name}</Text>
+      <Text preset='1.5em-normal' style={{ opacity: 0.5 }}>
+        {description}
+      </Text>
       {children}
     </FlexColumn>
   );
