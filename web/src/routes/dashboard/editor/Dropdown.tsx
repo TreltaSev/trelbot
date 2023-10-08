524d078ce355e60675f09a2d1cd2a7947dfc0b4a
TreltaSev <treltasev@gmail.com>
2023-10-08 00:50:48 -0400

    Add/file:Dropdown.tsx

---
+++ web/src/routes/dashboard/editor/Dropdown.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { uuidv4 } from "uuidv7";
+
+type props = {};
+
+type state = {};
+
+/**
+ * A animated dropdown that can display and hold information, animated in every
+ * way that countes
+ * @usedin `Editor.tsx/**`
+ * + display items
+ * + search interface
+ * + select items
+ * + search filters
+ */
+class Dropdown extends React.Component<props, state> {
+
+}
