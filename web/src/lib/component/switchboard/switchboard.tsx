23b266dcd7efade9b0be03d004c86b7db0be0a29
TreltaSev <treltasev@gmail.com>
2023-11-22 20:07:21 -0500

    Added Absolute Positioning

--- web/src/lib/component/switchboard/switchboard.tsx
+++ web/src/lib/component/switchboard/switchboard.tsx
@@ -10,12 +10,12 @@ class SwitchBoard extends Stylist {
   private is_visible: boolean = false;
 
   render() {
-    this.set_decor("parent", { width: 800, height: 50, gap: 40, borderRadius: 10 }, `${styling.justify_content_center} ${styling.align_items_center} ${styling.main}`);
+    this.set_decor("swb_parent", { width: 800, height: 50, gap: 40, borderRadius: 10, position: "absolute", bottom: 50 }, `${styling.justify_content_center} ${styling.align_items_center} ${styling.main}`);
 
     return (
       <>
         {/* Bar Parent */}
-        <FlexRow {...this.get_decor("parent")}>Testing</FlexRow>
+        <FlexRow {...this.get_decor("swb_parent")}>Testing</FlexRow>
       </>
     );
   }
