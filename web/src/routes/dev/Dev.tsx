656193f588d8c6d1219a880e6b2c767ad1a3c907
TreltaSev <treltasev@gmail.com>
2023-11-30 22:09:16 -0500

    TD Animation

--- web/src/routes/dev/Dev.tsx
+++ web/src/routes/dev/Dev.tsx
@@ -14,11 +14,27 @@ class Dev extends Stylist {
   private gathering_ref = React.createRef<LoadingText>();
   private loading_tsx_ref = React.createRef<LoadingText>();
   private dots_ref = React.createRef<LoadingText>();
+  private switchboard_reference = React.createRef<Switchboard>();
 
   componentDidMount(): void {
+    this.switchboard_reference.current?.hide(true);
     setTimeout(() => {
       this.gathering_ref.current?.hide();
     }, 3000);
+  }
+
+  CycleTDEnd(iteration: number) {
+    // Cycles Triple Dot End
+    if (iteration == 2) {
+      this.dots_ref.current!.stop_signal = true;
+    }
+
+    if (iteration == 3) {
+      this.loading_tsx_ref.current?.hide();
+      setTimeout(() => {
+        this.switchboard_reference.current?.show();
+      }, 1000);
+    }
   }
 
   render() {
@@ -29,9 +45,17 @@ class Dev extends Stylist {
           <FlexRow>
             <LoadingText duration={500.0} ref={this.gathering_ref} achieve_text='Gathering Permissions' type='run_on_render' hide_callback={() => this.loading_tsx_ref.current?.call()} />
             <LoadingText duration={500.0} ref={this.loading_tsx_ref} achieve_text='Loading TSX Components' type='run_on_call' show_callback={() => this.dots_ref.current?.call()} />
-            <LoadingText duration={500.0} ref={this.dots_ref} achieve_text='...' type='loop_when_called' />
+            <LoadingText
+              duration={500.0}
+              ref={this.dots_ref}
+              achieve_text='...'
+              type='loop_when_called'
+              end_callback={(iteration) => {
+                this.CycleTDEnd(iteration);
+              }}
+            />
           </FlexRow>
-          <Switchboard />
+          <Switchboard ref={this.switchboard_reference} />
         </FlexColumn>
       </NavigationLayout>
     );
