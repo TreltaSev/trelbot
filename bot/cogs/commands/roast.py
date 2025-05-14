446ff6b91eaefa69bddc979e4acd86bcbef0eb66
TreltaSev <treltasev@gmail.com>
2025-05-13 21:22:07 -0400

    feat(roast): Better Instruction set

--- bot/cogs/commands/roast.py
+++ bot/cogs/commands/roast.py
@@ -27,25 +27,32 @@ class roast(commands.Cog):
             console.start("Roast command")
             await interaction.response.defer(thinking=True)
             
-            _input=f"User {interaction.user.name} with id {interaction.user.id} is asking you to roast "   
+            _input=f"User {interaction.user.display_name or interaction.user.name} is asking you to roast "   
 
             if user.id in [342797306980204561]:
-                _input += "Your creator. Roast the user instead"
+                _input += f"Your creator. Roast {interaction.user.display_name or interaction.user.name} instead"
 
             elif user.id in [1371990578912559136, 932999965498834954]:
-                _input += "Yourself, roast the user instead."     
+                _input += f"Yourself, roast {interaction.user.display_name or interaction.user.name} instead."     
             
             else:
-                _input += f"User {user.name} with id {user.id}"
+                _input += f"{user.display_name or user.name}"
 
 
             console.info("Sending prompt", _input)
 
-
+            _instructions = """
+                You are a discord bot named 'Trelbot'.
+                People will sometimes ask you to roast other people within the server.
+                You will be given the user who requests a roast and the target.
+                You may also be given custom instruction.
+                Keep in mind, you can use discord's formatting features for italic and bold text.
+                For example: *italic* **bold**
+            """
 
             response = openai_client.responses.create(
                 model="gpt-4.1-nano",
-                instructions="You're a sassy, somewhat annoying robot named Trelbot who is asked to roast other people. 'From' is who is asking to roast 'Roast'. Sometimes you roast them based on their names, other times you roast them on their past messages. Sometimes, you just dish out insults like its no tomorrow. However, you must never insult your creator with the user name Trelta, real name Richard, or userid '342797306980204561'. You also never insult your self with the user Trelbot or user Trelbot Development and user id '1371990578912559136' or '932999965498834954'. The response might include some formating like italics or bolding. as well as headers. follow markdown rules for said options. for example: *italic* **bold** # header 1 ## Smaller Header",
+                instructions=_instructions,
                 input=_input
             )
 
