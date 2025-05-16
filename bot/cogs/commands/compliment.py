2f76688f8101d6550ddae99d0a3ad99067800138
TreltaSev <treltasev@gmail.com>
2025-05-15 22:50:39 -0400

    feat(compliment): Add compliment command

---
+++ bot/cogs/commands/compliment.py
@@ -0,0 +1,53 @@
+"""
+---
+cog: compliment
+"""
+
+import discord
+from discord.ext import commands
+from pyucc import console
+from utils.client import Client
+from utils.checks.openai import openai_check
+
+
+class compliment(commands.Cog):
+
+    def __init__(self, client: Client) -> None:
+        self.client = client
+
+    @discord.app_commands.command()
+    @openai_check
+    async def compliment(self, interaction: discord.Interaction, user: discord.User):
+
+        await interaction.response.defer(thinking=True)
+
+        _input = f"User {interaction.user.display_name or interaction.user.name} is asking you to compliment"
+
+        if interaction.user.id in [342797306980204561] and user.id in [342797306980204561]:
+            _input = "Your creator is asking you to compliment himself..."
+
+        elif interaction.user.id in [342797306980204561] and user.id in [1371990578912559136, 932999965498834954]:
+            _input = "Your creator is asking you to compliment yourself..."
+
+        else:
+            _input += f"{user.display_name or user.name}"
+
+        console.info("Sending prompt", _input)
+
+        _instructions = """
+                You are a discord bot named 'Trelbot'.
+                People will sometimes ask you to compliment other people within the server.
+                You will be given the user who requests a compliment and the target.
+                You may also be given custom instruction.
+                Your creator is a he/him with the real name Richard, user name Trelta, User id 342797306980204561
+                Keep in mind, you can use discord's formatting features for italic and bold text.
+                For example: *italic* **bold**
+            """
+
+        response = self.client.openai.responses.create(
+            model="gpt-4.1-nano",
+            instructions=_instructions,
+            input=_input
+        )
+
+        await interaction.followup.send(content=response.output_text)
